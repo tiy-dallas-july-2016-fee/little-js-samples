6d973c22f0b114f646fb3e0de6b0bc8c4a3fb57e
Eric Sowell <eric.sowell@gmail.com>
2016-08-20 06:23:19 -0500

    dogs sample

--- day-30-js-types/dogs.js
+++ day-30-js-types/dogs.js
@@ -1,1 +1,39 @@
-console.log('hello');
+//Base type
+function Dog() {
+}
+
+Dog.prototype.bark = function() {
+  console.log('ruff ruff!');
+};
+
+function Labrador() { }
+Labrador.prototype = new Dog();
+
+function Chiahuahua() {}
+Chiahuahua.prototype = new Dog();
+//Overriding the bark function, ONLY on Chiahuahua
+Chiahuahua.prototype.bark = function() {
+  console.log('yip!');
+}
+
+function Terrier() {}
+Terrier.prototype = new Dog();
+
+//An instance of Labrador
+var fido = new Labrador();
+
+fido.bark();
+
+var spot = new Chiahuahua();
+spot.bark();
+
+var rover = new Terrier();
+rover.bark();
+
+console.log('rover terrier?', rover instanceof Terrier);
+console.log('rover array?', rover instanceof Array);
+console.log('rover dog?', rover instanceof Dog);
+
+
+var dude = { name: 'Fella' };
+var objectDog = { name: 'Rover' };
